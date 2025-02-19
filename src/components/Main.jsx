91ed10049859f2cf8bf1a038f8768a1dbed0849a
Alec German <kimarimt@protonmail.com>
2025-02-18 20:19:13 -0500

    finished 10.3

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,21 @@
-import { Text } from 'react-native'
+import Constants from 'expo-constants'
+import { StyleSheet, SafeAreaView } from 'react-native'
+import RepositoryList from './RepositoryList'
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1
+  }
+})
 
 const Main = () => {
-  return <Text>Hello, World!</Text>
+  return (
+    <SafeAreaView style={styles.container}>
+      <RepositoryList />
+    </SafeAreaView>
+  )
 }
 
 export default Main
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,24 @@
+import { FlatList, View, StyleSheet } from 'react-native'
+import { REPOSITORIES } from '../data/repositoryData'
+import RepositoryItem from './RepositoryItem'
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  }
+})
+
+const ItemSeparator = () => <View style={styles.separator} />
+
+const RepositoryList = () => {
+  return (
+    <FlatList
+      data={REPOSITORIES}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={({ item }) => <RepositoryItem repository={item} />}
+      keyExtractor={item => item.id}
+    />
+  )
+}
+
+export default RepositoryList
