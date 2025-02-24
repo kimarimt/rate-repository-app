5e727cb65581f5648a27f8e3f80f4172c63ec30b
Alec German <kimarimt@protonmail.com>
2025-02-23 23:25:56 -0500

    finished 10.8

--- src/components/Pages/SignIn.jsx
+++ src/components/Pages/SignIn.jsx
@@ -1,4 +1,5 @@
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, TextInput, Pressable } from 'react-native'
+import { useFormik } from 'formik'
 import Text from '../util/Text'
 import theme from '../../theme'
 
@@ -6,13 +7,70 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colors.onPrimary
-  }
+  },
+  form: {
+    marginHorizontal: 15,
+  },
+  input: {
+    height: 50,
+    marginTop: 20,
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: theme.colors.textSecondary,
+    padding: 10
+  },
+  button: {
+    backgroundColor: theme.colors.primary,
+    height: 50,
+    borderRadius: 2,
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 })
 
+const initialValues = {
+  username: '',
+  password: ''
+}
+
 const SignIn = () => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit
+  })
+
+  const onSubmit = values => {
+    console.log(values)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>The sign-in-view</Text>
+      <View style={styles.form}>
+        <TextInput
+          placeholder='Username'
+          value={formik.values.username}
+          onChangeText={formik.handleChange('username')}
+          autoCapitalize='none'
+          style={styles.input}
+        />
+        <TextInput
+          placeholder='Password'
+          value={formik.values.password}
+          onChangeText={formik.handleChange('password')}
+          style={styles.input}
+          secureTextEntry
+        />
+        <Pressable style={styles.button} onPress={() => onSubmit(formik.values)}>
+          <Text
+            color='onPrimary'
+            fontWeight='bold'
+            fontSize='subheading'
+          >
+            Sign in
+          </Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
