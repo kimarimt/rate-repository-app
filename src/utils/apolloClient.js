c110319d01a18f58a468d86eeb538347be6db194
Alec German <kimarimt@protonmail.com>
2025-02-25 14:57:39 -0500

    finished 10.12

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
+import Constants from 'expo-constants'
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://192.168.1.103:4000/api/repositories',
+    uri: Constants.expoConfig.extra.apolloUri,
     cache: new InMemoryCache()
   })
 }
